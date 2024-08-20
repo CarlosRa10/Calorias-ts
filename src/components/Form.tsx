8dbb39523086be05f44c66cf01f5b81e7ed1fd2e
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-08-20 16:56:21 -0400

    commit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react"
+import { useState,ChangeEvent } from "react"
+import { Activity } from "../types"
 import { categories } from "../data/categories"
 export default function Form() {
-    const [activity,setActivity]=useState({
+    //definiendo nuestro state y conectarlo a los diferentes inputs
+    const [activity,setActivity]=useState<Activity>({//setActivity es un objeto
         category:1,
         name:'',
         calories:0
     })
+    
+//actualizar el value de el input
+    const handleChange = (e:ChangeEvent<HTMLSelectElement>|ChangeEvent<HTMLInputElement>) =>{//si no le especificamos el tipo de dato da eny
+        const isNumberField = ['category','calories'].includes(e.target.id)//include es un array metodo-- si estoy escribiendo en categoria o en caloria me arroja un true, si no un false
+        //console.log(isNumberField)
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity =()=>{
+        const {name,calories} = activity
+        return name.trim() !== '' && calories > 0//El método trim( ) elimina los espacios en blanco en ambos extremos del string.
+    }
+
   return (
     <form
         className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -18,11 +37,12 @@ export default function Form() {
                 className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                 id="category"
                 value={activity.category}
+                onChange={handleChange}
             >
                 {categories.map(category =>(
                     <option
                         key={category.id}
-                        value={category.id}
+                        value={category.id}//sincronizar mi state con la seleccion , pero para que se refleje los cambios tengo que tener un onChange
                     >
                         {category.name}
                     </option>
@@ -40,6 +60,7 @@ export default function Form() {
             className="border border-slate-300 p-2 rounded-lg "
             placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
             value={activity.name}
+            onChange={handleChange}
            />
         </div>
 
@@ -53,13 +74,15 @@ export default function Form() {
             className="border border-slate-300 p-2 rounded-lg "
             placeholder="Calorias. ej. 300 o 500"
             value={activity.calories}
+            onChange={handleChange}
            />
         </div>
 
         <input 
             type="submit" 
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-black uppercase text-white cursor-pointer"
-            value={'Guardar comida o guardar ejercicio'}
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-black uppercase text-white cursor-pointer disabled:opacity-10"
+            value={activity.category === 1?'Guardar Comida':'Guardar Ejercicio'}
+            disabled={!isValidActivity()}
         />
     </form>
   )
